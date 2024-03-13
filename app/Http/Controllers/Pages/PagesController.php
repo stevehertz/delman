25582f7f887367c8d701f9ac07ae4f493eea688c
unknown <info@schedulize.co.ke>
2024-03-13 19:50:45 +0300

    Project setup

---
+++ app/Http/Controllers/Pages/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //
+    public function index()  
+    {
+        
+    }
+}
